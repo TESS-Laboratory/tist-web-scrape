3355ff5b24697de6c022d3f6e66f073161be31dc
Hugh Graham <hugh.a.graham@gmail.com>
2024-04-22 20:15:34 +0100

    iterate files in R. add commented code for testing .

--- tist-web-scrape-run.R
+++ tist-web-scrape-run.R
@@ -11,9 +11,13 @@ library(janitor)
 library(measurements)
 library(glue)
 
+options(warn = 2)
+
 # --- read the functions that are in the R directory ---
-source("R/tist-web-scrape-functions.R")
-source("R/build_tables.R")
+purrr::walk(
+  list.files("R", pattern = "\\.R$", full.names = TRUE),
+  source
+)
 
 # --- This is the pipeline for Uganda ---
 # first get the url links for all projects in a country
@@ -22,6 +26,20 @@ ug_proj_urls <- project_urls("uganda")
 ug_cluster_urls <- cluster_urls(ug_proj_urls)
 # then the url links for the groups in each cluster
 ug_group_urls <- group_urls(ug_cluster_urls)
+
+## --- for testing specific issue: ---
+# ug_group_urls_test <- ug_group_urls[1:100]
+# # ug_group_urls_test <- "https://www.tist.org/i2//detgroup.php?groupid=16396"
+# attr(ug_group_urls_test, "country") <- "uganda"
+# ug_all_test <- get_all_group_tabs(ug_group_urls_test)
+# ug_all_test[[2]]$geometry
+# ply_test <- clean_up_spat(ug_all_test[[2]])
+# pnt_test <- clean_up_spat(ug_all_test[[1]])
+
+# mapview::mapview(ply_test) +
+#   mapview::mapview(pnt_test)
+
+
 # Scrape the data from each cluster
 # (this takes a long time ~2hrs for Uganda)
 ug_all <- get_all_group_tabs(ug_group_urls)
