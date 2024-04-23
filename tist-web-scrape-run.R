ed1d360b950d17e2feb86413b4c9cd852d6365db
Hugh Graham <hugh.a.graham@gmail.com>
2024-04-23 11:01:30 +0100

    add map creation to run script

--- tist-web-scrape-run.R
+++ tist-web-scrape-run.R
@@ -10,8 +10,9 @@ library(purrr)
 library(janitor)
 library(measurements)
 library(glue)
+library(sf)
 
-options(warn = 2)
+# options(warn = 0)
 
 # --- read the functions that are in the R directory ---
 purrr::walk(
@@ -27,21 +28,16 @@ ug_cluster_urls <- cluster_urls(ug_proj_urls)
 # then the url links for the groups in each cluster
 ug_group_urls <- group_urls(ug_cluster_urls)
 
-## --- for testing specific issue: ---
-# ug_group_urls_test <- ug_group_urls[1:100]
-# # ug_group_urls_test <- "https://www.tist.org/i2//detgroup.php?groupid=16396"
-# attr(ug_group_urls_test, "country") <- "uganda"
-# ug_all_test <- get_all_group_tabs(ug_group_urls_test)
-# ug_all_test[[2]]$geometry
-# ply_test <- clean_up_spat(ug_all_test[[2]])
-# pnt_test <- clean_up_spat(ug_all_test[[1]])
-
-# mapview::mapview(ply_test) +
-#   mapview::mapview(pnt_test)
-
-
 # Scrape the data from each cluster
 # (this takes a long time ~2hrs for Uganda)
 ug_all <- get_all_group_tabs(ug_group_urls)
 # Finally save those outputs.
 ug_files <- save_group_tabs(ug_all)
+
+# view polygons and points on a map.
+# we must clean the spatial data to view on a leaflet map otherwise things crash.
+ug_polys <- clean_up_spat(read_sf("TIST-data/uganda_grove_polys.gpkg"))
+ug_points <- clean_up_spat(read_sf("TIST-data/uganda_grove_points.gpkg"))
+
+mapview::mapview(ug_polys) +
+  mapview::mapview(ug_points)
