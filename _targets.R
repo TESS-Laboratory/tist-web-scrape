3a7160a2f3856d0acf5f810ee4ef7b49bc2eb1d2
Hugh Graham <hugh.a.graham@gmail.com>
2024-06-28 10:50:56 +0100

    add targets script

---
+++ _targets.R
@@ -0,0 +1,64 @@
+# The targets script to run the TIST web scraping pipeline.
+# to run this pipeline use any of:
+# `targets::tar_make()`, source("targets-run.R"), or `make`.
+# `make` is preferred as this method will run the pipeline in the
+# background and therefore can be left to run after disconnecting from ssh.
+
+# Load packages required to define the pipeline:
+library(targets)
+library(tarchetypes) # Load other packages as needed.
+library(tibble)
+# Set target options:
+tar_option_set(
+  # Packages for the pipeline
+  packages = c(
+    "tibble", "rvest", "dplyr", "tibble", "stringr", "purrr",
+    "janitor", "measurements", "glue", "sf", "cli"
+  ),
+  format = "qs", # use qs
+)
+
+# Run the R scripts in the R/ folder with your custom functions:
+tar_source()
+
+
+values <- tibble(
+  countries = c("uganda", "kenya", "tanzania", "india")
+)
+
+
+# Replace the target list below with your own:
+list(
+  tar_map(
+    values,
+    tar_target(
+      get_proj_urls,
+      project_urls(countries)
+    ),
+    tar_target(
+      get_cluster_urls,
+      cluster_urls(get_proj_urls)
+    ),
+    tar_target(
+      get_group_urls,
+      group_urls(get_cluster_urls)
+    ),
+    tar_target(
+      group_tabs,
+      get_group_tables(get_group_urls, countries),
+      pattern = map(get_group_urls),
+      iteration = "list"
+    ),
+    tar_target(
+      combined_group_tabs,
+      command = {
+        purrr::transpose(group_tabs) |>
+          purrr::map(dplyr::bind_rows)
+      }
+    ),
+    tar_target(
+      save_tabs,
+      save_group_tabs(combined_group_tabs)
+    )
+  )
+)
